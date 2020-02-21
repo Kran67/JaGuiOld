e8f61565ade270ef327ed31d2493d75f41851d02
Mig <lopesmiguel80@gmail.com>
2020-02-21 22:34:11 +0100

    Add template

--- scripts/components/extended/textboxbtn.js
+++ scripts/components/extended/textboxbtn.js
@@ -19,46 +19,14 @@ class TextBoxBtn extends CustomTextBoxBtn {
     //#endregion Methods
 }
 //#endregion TextBoxBtn
+Object.seal(TextBoxBtn);
 Core.classes.register(Types.CATEGORIES.EXTENDED, TextBoxBtn);
 //#region Templates
 if (Core.isHTMLRenderer) {
-    var TextBoxBtnTpl = "<div id='{internalId}' data-name='{name}' data-class='TextBoxBtn' class='Control TextBox TextBoxBtn {theme}'>\
-                       <input type='text' class='Control csr_text TextBoxInput TextBoxBtnInput {theme}'>\
-                       {buttons}\
-                       </div>";
+    var TextBoxBtnTpl = ['<jagui-textboxbtn id="{internalId}" data-class="TextBoxBtn" class="Control TextBox',
+        ' TextBoxBtn {theme}"><properties>{ "name": "{name}", "left": 1120, "top": 370, "width": 121, "height": 21 }',
+        '</properties></jagui-textboxbtn>'].join(StringEMPTY);
     Core.classes.registerTemplates([{ Class: TextBoxBtn, template: TextBoxBtnTpl }]);
 }
 //#endregion Templates
 export { TextBoxBtn };
-
-/*(function() {
-    //#region TextBoxBtn
-    var TextBoxBtn = $j.classes.CustomTextBoxBtn.extend("TextBoxBtn",{
-        init: function(owner,props) {
-            if(owner) {
-                props = !props ? {} : props;
-                props._btnClass = $j.classes.TextButton;
-                this._inherited(owner,props);
-                //#region Private
-                //#endregion
-                if(!$j.isHTMLRenderer()) {
-                    this.width = 121;
-                    this.height = 21;
-                }
-            }
-        }
-        //#region Setters
-        //#endregion
-        //#region Methods
-    });
-    $j.classes.register($j.types.categories.EXTENDED,TextBoxBtn);
-    //#region Templates
-    if($j.isHTMLRenderer()) {
-        var TextBoxBtnTpl = "<div id='{internalId}' data-name='{name}' data-class='TextBoxBtn' class='Control TextBox TextBoxBtn {theme}'>\
-                       <input type='text' class='Control csr_text TextBoxInput TextBoxBtnInput {theme}'>\
-                       {buttons}\
-                       </div>";
-        $j.classes.registerTemplates([{ Class: TextBoxBtn,template: TextBoxBtnTpl }]);
-    }
-    //endregion
-})();*/
