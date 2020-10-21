aa38255b90df29952fb47b508da8518688f551af
Mig <lopesmiguel80@gmail.com>
2020-10-21 17:42:08 +0200

    Virtual mode for ListBox2 & sizeOf method

--- scripts/core/tools.js
+++ scripts/core/tools.js
@@ -612,6 +612,53 @@ class Tools {
         return dayList.map(getDayName);
     }
     //#endregion getWeekDayList
+    //#region sizeOf
+    static sizeOf(object) {
+        // initialise the list of objects and size
+        var objects = [object];
+        var size = 0;
+        // loop over the objects
+        for (var index = 0; index < objects.length; index++) {
+            // determine the type of the object
+            switch (typeof objects[index]) {
+                // the object is a boolean
+                case 'boolean':
+                    size += 4;
+                    break;
+                // the object is a number
+                case 'number':
+                    size += 8;
+                    break;
+                // the object is a string
+                case 'string':
+                    size += 2 * objects[index].length;
+                    break;
+                // the object is a generic object
+                case 'object':
+                    // if the object is not an array, add the sizes of the keys
+                    if (Object.prototype.toString.call(objects[index]) != '[object Array]') {
+                        for (var key in objects[index]) size += 2 * key.length;
+                    }
+                    // loop over the keys
+                    for (var key in objects[index]) {
+                        // determine whether the value has already been processed
+                        var processed = false;
+                        for (var search = 0; search < objects.length; search++) {
+                            if (objects[search] === objects[index][key]) {
+                                processed = true;
+                                break;
+                            }
+                        }
+                        // queue the value to be processed if appropriate
+                        !processed && objects.push(objects[index][key]);
+                    }
+            }
+        }
+        // return the calculated size
+        return size;
+    }
+
+    //#endregion sizeOf
     //#region defineLayout
     //static defineLayout(control, props) {
     //    if (control.update) {
@@ -784,7 +831,7 @@ class Tools {
         event.stopImmediatePropagation();
         event.preventDefault();
     }
-//#endregion Methods
+    //#endregion Methods
 }
 Object.seal(Object.freeze(Tools));
 //#endregion Tools
